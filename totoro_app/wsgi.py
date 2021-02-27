0631726bada80986f81c770aee697ccd23acef0f
Abhishek Juneja <abhishek.juneja145@gmail.com>
2021-02-27 19:05:18 +0530

    tweaks to wsgi and manage.py

--- totoro_app/wsgi.py
+++ totoro_app/wsgi.py
@@ -1,9 +1,9 @@
 import os
 
-from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "totoro_app.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
